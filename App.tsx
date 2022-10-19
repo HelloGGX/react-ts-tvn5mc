0c7e3444e034651687aeed61999a41788c9c476f
HelloGGX <1056937524@qq.com>
2022-10-19 10:40:59 +0800

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+import * as THREE from 'three';
+
+export default function App() {
+  // const scene = new THREE.
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-tvn5mc
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/HelloGGX/react-ts-tvn5mc)
